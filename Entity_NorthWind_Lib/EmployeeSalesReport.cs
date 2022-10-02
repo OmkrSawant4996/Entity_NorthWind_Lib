5380b129cdad2086d19743599682b9c08f9c30ec
Omkar <Omkar@DESKTOP-L64HDIE>
2022-10-02 22:50:36 +0530

    Add project files.

---
+++ Entity_NorthWind_Lib/EmployeeSalesReport.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Entity_NorthWind_Lib
+{
+    internal class EmployeeSalesReport
+    {
+        public static void SalesReport()
+        {
+            using (var db = new NorthwindEntities())
+            {
+                var salesReport = db.EmployeeSalesReport()
+
+                foreach (var item in result1)
+                {
+                    Console.WriteLine($"{item.userId}\t\t{item.FirstName}\t\t{item.LastName}");
+                }
+
+                Console.WriteLine("\n\n");
+                Console.WriteLine("======Group======");
+
+                Group g = new Group();
+                g.Name = "D";
+                db.Groups.Add(g);
+                db.SaveChanges();
+                var result3 = db.Groups.ToList();
+
+                foreach (Group item in result3)
+                {
+                    Console.WriteLine(item.Id + "\t\t" + item.Name);
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
